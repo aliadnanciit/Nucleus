be59fe45dec58d365fee33644b31726c3e1c5704
aliadnanciit <aliadnanciit@gmail.com>
2024-10-10 14:33:49 +0400

    Update Package.swift
    update url

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "Nucleus",
-         url: "https://github.com/aliadnanciit/Nucleus/releases/download/Nucleus-tag1/Nucleus.xcframework.zip",
+         url: "https://github.com/aliadnanciit/Nucleus/releases/download/v1.2/Nucleus.xcframework.zip",
          checksum:"b4136635bd9be125e1fa9cf929b44024baca4e77d4f6bd0601eb1a18b7860cb0")
    ]
 )
